471a34ef776a840e26be65541c742dc77c705e3c
BuildTools <unconfigured@null.spigotmc.org>
2023-02-05 13:53:44 +0300

    Code layout has been improved

--- user_interface.py
+++ user_interface.py
@@ -25,12 +25,12 @@ def set_theme(mode):
 def ok_command():
     global youtube_video
     try:
-        youtube_video = downloader.video(
-            entry.get(), on_progress_callback=on_progress, on_complete_callback=finish_download)
+        youtube_video = downloader.video(entry.get(
+        ), on_progress_callback=on_progress, on_complete_callback=finish_download)
     except exceptions.RegexMatchError:
         if not entry.get() == 'thank you':
             messagebox.showerror(
-                title='Unvalid URL!', message='The url you entered is invalid. Please make sure you typed the url correctly.')
+                'Unvalid URL!', 'The url you entered is invalid. Please make sure you typed the url correctly.')
         else:
             messagebox.showinfo('Seminet:', "It's my pleasure. :)")
         return False
@@ -133,14 +133,13 @@ def finish_download(a, b):
 
 
 def download_command():
+    global progress_bar, bytes_remained, download_ok_button, progress_window
     streaming_thread.join()
     loading_thread.join()
-    global progress_bar, bytes_remained, download_ok_button, progress_window
-    progress_window = ctk.CTkToplevel(root)
 
+    progress_window = ctk.CTkToplevel(root)
     progress_window.title('Download Progress')
     progress_window.resizable(False, False)
-
     w = 430
     h = 96
     progress_window.geometry(
@@ -148,7 +147,6 @@ def download_command():
 
     label_frame = ctk.CTkFrame(
         master=progress_window, fg_color='transparent', border_width=0)
-
     download_label = ctk.CTkLabel(
         master=label_frame, text='Downloading Video:')
     bytes_label = ctk.CTkLabel(
@@ -158,8 +156,7 @@ def download_command():
     download_ok_button = ctk.CTkButton(
         master=progress_window, text='OK', state='disabled', command=progress_window.destroy)
     progress_bar.set(0)
-    label_frame.grid(row=0, column=0, columnspan=2,
-                     padx=10, pady=5)
+    label_frame.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
     download_label.grid(row=0, column=0, padx=5)
     bytes_label.grid(row=0, column=1, padx=5)
     progress_bar.grid(row=1, column=0, columnspan=3, padx=15)
@@ -177,16 +174,13 @@ ctk.set_default_color_theme('red_theme.json')
 
 root = ctk.CTk()
 
-
 url = ctk.StringVar()
-url.set('https://www.youtube.com/watch?v=MNeX4EGtR5Y&ab_channel=Fireship')
-title = ctk.StringVar(
-    value='SeminetScienceInnovatorsYoutubeVideoDownloader')
-channel = ctk.StringVar(value='By Seminet')
-length = ctk.StringVar(value='4:51')
-resolution = ctk.StringVar(value='720p')
+url.set('https://www.youtube.com/watch?v=a3Kd4MRaRAw&ab_channel=GitHub')
+title = ctk.StringVar()
+channel = ctk.StringVar()
+length = ctk.StringVar()
 file_size = ctk.StringVar()
-loading_text = ctk.StringVar(value='')
+loading_text = ctk.StringVar()
 bytes_completed = ctk.StringVar()
 
 
@@ -195,41 +189,37 @@ root.withdraw()
 
 main_window = ctk.CTkToplevel(root)
 
-main_window.iconbitmap('appicon.ico')
-
 main_window.protocol('WM_DELETE_WINDOW', root.destroy)
-main_window.title('Youtube Video Downloader | Seminet Science Innovators')
+main_window.title('Youtube Video Downloader')
 x = 660
 y = 270
 main_window.geometry(
     f'{x}x{y}+{int((root.winfo_screenwidth()-x)/2)}+{int((root.winfo_screenheight()-y)/2)}')
 main_window.resizable(width=False, height=False)
-
 main_window.columnconfigure(1, weight=1)
 
 entry = ctk.CTkEntry(master=main_window, font=('Roboto', 12), textvariable=url)
-
 information = ctk.CTkFrame(master=main_window)
 information.columnconfigure(1, weight=1)
 
-
+# Getting Information
 first_video = downloader.video(url.get())
-
+title.set(first_video.title)
+channel.set(first_video.author)
+length.set(first_video.length_str())
 photo = first_video.thumbnail_image()
+
+# Information Section
 thumbnail_label = tk.Label(master=information, image=photo)
 thumbnail_label.image = photo
-
-
-title_label = ctk.CTkLabel(
-    master=information, textvariable=title, font=('Roboto', 14), justify=tk.LEFT, wraplength=190)
-
+title_label = ctk.CTkLabel(master=information, textvariable=title, font=(
+    'Roboto', 14), justify=tk.LEFT, wraplength=190)
 channel_label = ctk.CTkLabel(
     master=information, textvariable=channel, font=('Roboto', 12))
-
 length_label = ctk.CTkLabel(
     master=information, textvariable=length, font=('Roboto', 12))
 
-
+# Information Grid
 thumbnail_label.grid(row=0, column=0, rowspan=2,
                      padx=(12, 5), pady=10, sticky=tk.N+tk.W)
 title_label.grid(row=0, column=1, columnspan=2,
@@ -237,7 +227,7 @@ title_label.grid(row=0, column=1, columnspan=2,
 channel_label.grid(row=1, column=1, padx=5, pady=5, sticky=tk.S+tk.W)
 length_label.grid(row=1, column=2, padx=(5, 15), pady=5, sticky=tk.S+tk.E)
 
-
+# Button Section
 button_frame = ctk.CTkFrame(
     master=main_window, fg_color='transparent', border_width=0)
 ok_button = ctk.CTkButton(master=button_frame, text='OK', command=ok_command)
@@ -246,14 +236,14 @@ continue_button = ctk.CTkButton(
 theme_button = ctk.CTkOptionMenu(master=button_frame, values=[
                                  'Light Mode', 'Dark Mode'], anchor=ctk.CENTER, command=set_theme)
 theme_button.set('Dark Mode')
-
 loading_label = ctk.CTkLabel(master=main_window, textvariable=loading_text)
+
 # Download Section
 download_frame = ctk.CTkFrame(master=main_window)
 download_frame.grid_columnconfigure(2, weight=1)
 directory_button = ctk.CTkButton(
     master=download_frame, text='Select Folder', command=set_directory)
-resolutions_menu = ctk.CTkOptionMenu(master=download_frame, variable=resolution, values=[
+resolutions_menu = ctk.CTkOptionMenu(master=download_frame, values=[
                                      '144p', '240p', '360p', '480p', '720p'], anchor=ctk.CENTER, state='disabled', command=get_file_size)
 file_size_label = ctk.CTkLabel(master=download_frame, textvariable=file_size)
 download_button = ctk.CTkButton(
@@ -264,8 +254,7 @@ entry.grid(row=1, column=0, columnspan=5, padx=10,
            pady=(10, 5), sticky=tk.E+tk.W)
 information.grid(row=2, column=0, rowspan=2, columnspan=3,
                  padx=10, pady=5, sticky=tk.W+tk.N+tk.E)
-button_frame.grid(row=2, column=4,
-                  padx=10, pady=5, sticky=tk.N+tk.E)
+button_frame.grid(row=2, column=4, padx=10, pady=5, sticky=tk.N+tk.E)
 ok_button.grid(row=0, column=0, pady=5)
 continue_button.grid(row=1, column=0, pady=5)
 theme_button.grid(row=2, column=0, pady=5)
